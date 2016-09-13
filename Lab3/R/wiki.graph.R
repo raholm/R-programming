bfc0e972e2b662b4dbbb97cfde5d7b305752ad54
CosiFun515 <k.ziomek@web.de>
2016-09-13 16:32:42 +0200

    modifying description of data

--- Lab3/R/wiki.graph.R
+++ Lab3/R/wiki.graph.R
@@ -3,9 +3,9 @@
 #'
 #' @format A data frame with 3 vectors:
 #' \describe{
-#'   \item{v1}{testing.}
-#'   \item{v2}{testing..}
-#'   \item{w}{testing...}
+#'   \item{v1}{starting point of an edge}
+#'   \item{v2}{end point of an edge}
+#'   \item{w}{weight of the edge}
 #' }
 #' @source \url{http://www.diamondse.info/}
 wiki.graph <- data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
