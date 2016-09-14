0a81cce4032097db8654e47db68a6eaee0a1b637
raholm <rasmus.holm723@gmail.com>
2016-09-14 05:01:49 +0200

    Formatted euclidian file.

--- Lab3/R/euclidian.R
+++ Lab3/R/euclidian.R
@@ -1,22 +1,17 @@
-euclidian <- function(a, b)
-{
-  euclidian.check.input(a, b)
-  
-  while (b != 0)
-  {
-    tmp <- b
-    b <- a %% b
-    a <- tmp
-  }
-  
-  return(a)
+euclidian <- function(a, b) {
+    euclidian_check_input(a, b)
+
+    while (b != 0)
+    {
+        tmp <- b
+        b <- a %% b
+        a <- tmp
+    }
+
+    return(a)
 }
 
-euclidian.check.input <- function(a, b)
-{
+euclidian_check_input <- function(a, b) {
   stopifnot(is.numeric(a), length(a) == 1)
   stopifnot(is.numeric(b), length(b) == 1)
 }
-
-euclidian(123612, 13892347912)
-euclidian(100, 1000)
