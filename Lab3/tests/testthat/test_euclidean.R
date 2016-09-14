44b3408654b7f5ec625622418fe46a419d311263
Rasmus Holm <rasmus.holm723@gmail.com>
2016-09-14 16:36:03 +0200

    Updated euclidean algorithm to only work on positive integers and corresponding tests.

--- Lab3/tests/testthat/test_euclidean.R
+++ Lab3/tests/testthat/test_euclidean.R
@@ -4,7 +4,12 @@ context("Euclidean")
 test_that("euclidean of invalid input is invalid", {
   expect_error(euclidean("invalid", 1000))
   expect_error(euclidean(100, "invalid"))
-  expect_error(euclidean(c(100, 1000), c(1000, 100)))
+  expect_error(euclidean(c(100, 1000), 100))
+  expect_error(euclidean(100, c(100, 1000)))
+  expect_error(euclidean(1.5, 100))
+  expect_error(euclidean(100, 1.5))
+  expect_error(euclidean(-1, 100))
+  expect_error(euclidean(100, -1))
 })
 
 test_that("euclidean of valid input returns correct output", {
