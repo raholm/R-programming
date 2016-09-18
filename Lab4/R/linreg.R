88700d4d4fc0aff58f9190741aef527d4e6c8ee3
raholm <rasmus.holm723@gmail.com>
2016-09-18 19:43:14 +0200

    Updated the description. Added file for linreg and generated documentation.

---
+++ Lab4/R/linreg.R
@@ -0,0 +1,23 @@
+#' Title here
+#'
+#' description here
+#'
+#' @param param description
+#' @return
+#'
+#' @examples
+#' examples here
+#'
+#' @export
+#' @source \url{}
+linreg <- function(formula, data)
+{
+    linreg_check_input(formula, data)
+
+    
+}
+
+linreg_check_input <- function(formula, data)
+{
+    
+}
