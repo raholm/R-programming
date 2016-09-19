ffd7d03bd0c9d7eee170793a75ec73fd998371a5
raholm <rasmus.holm723@gmail.com>
2016-09-19 16:17:24 +0200

    Added more tests and divided them into simple and advanced models.

--- Lab4/tests/testthat/test_linreg.R
+++ Lab4/tests/testthat/test_linreg.R
@@ -1,17 +1,15 @@
 library(Lab4)
 context("linreg")
 
-test_that("linreg of invalid input is invalid", {
-    data(iris)
+data(iris)
 
+test_that("linreg of invalid input is invalid", {
     expect_error(linreg(1, iris))
     expect_error(linreg(y ~ x, 1))
     expect_error(linreg(y ~ x, iris))
 })
 
-test_that("linreg of valid input is correct", {
-    data(iris)
-
+test_that("linreg of valid input is correct (simple model)", {
     model.expected <- lm(Petal.Width ~ Petal.Length, data=iris)
     model.actual <- linreg(Petal.Width ~ Petal.Length, data=iris)
 
@@ -22,9 +20,22 @@ test_that("linreg of valid input is correct", {
     expect_equal(model.actual$df, model.expected$df.residual)
 })
 
-test_that("linreg coefficient statistics are correct", {
-    data(iris)
+test_that("linreg of valid input is correct (advanced model)", {
+    model.expected <- lm(Petal.Width ~ Petal.Length + Sepal.Width + Sepal.Length, data=iris)
+    model.actual <- linreg(Petal.Width ~ Petal.Length + Sepal.Width + Sepal.Length, data=iris)
 
+    ## The string has been divided into multiple string due to its length.
+    ## This puts it back to a single string.
+    call_string <- gsub(" +", " ", paste(deparse(model.actual$call), collapse=""))
+    expect_equal(call_string,
+                 "linreg(formula = Petal.Width ~ Petal.Length + Sepal.Width + Sepal.Length, data = iris)")
+    expect_equal(model.actual$coef(), model.expected$coefficients)
+    expect_equal(model.actual$resid(), model.expected$residuals)
+    expect_equal(model.actual$pred(), predict(model.expected))
+    expect_equal(model.actual$df, model.expected$df.residual)
+})
+
+test_that("linreg coefficient statistics are correct (simple model)", {
     model.expected <- lm(Petal.Width ~ Petal.Length, data=iris)
     coefficients <- summary(model.expected)$coefficients
     model.actual <- linreg(Petal.Width ~ Petal.Length, data=iris)
@@ -33,4 +44,16 @@ test_that("linreg coefficient statistics are correct", {
     expect_equal(model.actual$coefficients$se, coefficients[, 2])
     expect_equal(model.actual$coefficients$tval, coefficients[, 3])
     expect_equal(model.actual$coefficients$pval, coefficients[, 4])
+})
+
+test_that("linreg coefficient statistics are correct (advanced model)", {
+    model.expected <- lm(Petal.Width ~ Petal.Length + Sepal.Width + Sepal.Length, data=iris)
+    coefficients <- summary(model.expected)$coefficients
+    model.actual <- linreg(Petal.Width ~ Petal.Length + Sepal.Width + Sepal.Length, data=iris)
+
+    expect_equal(model.actual$coefficients$val, coefficients[, 1])
+    expect_equal(model.actual$coefficients$se, coefficients[, 2])
+    expect_equal(model.actual$coefficients$tval, coefficients[, 3])
+    expect_equal(model.actual$coefficients$pval, coefficients[, 4])
+
 })
