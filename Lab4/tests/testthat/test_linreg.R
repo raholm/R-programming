1b45215d96b7feb21c08aec84da0f3f6e25c07d4
raholm <rasmus.holm723@gmail.com>
2016-09-19 13:21:49 +0200

    Added tests that check the statistics of the coefficients.

--- Lab4/tests/testthat/test_linreg.R
+++ Lab4/tests/testthat/test_linreg.R
@@ -19,4 +19,18 @@ test_that("linreg of valid input is correct", {
     expect_equal(model.actual$coef(), model.expected$coefficients)
     expect_equal(model.actual$resid(), model.expected$residuals)
     expect_equal(model.actual$pred(), predict(model.expected))
+    expect_equal(model.actual$df, model.expected$df.residuals)
+})
+
+test_that("linreg coefficient statistics are correct", {
+    data(iris)
+
+    model.expected <- lm(Petal.Width ~ Petal.Length, data=iris)
+    coefficients <- summary(model.expected)$coefficients
+    model.actual <- linreg(Petal.Width ~ Petal.Length, data=iris)
+
+    expect_equal(model.actual$coefficients, coefficients[, 1])
+    expect_equal(model.actual$coefficients.se, coefficients[, 2])
+    expect_equal(model.actual$coefficients.tval, coefficients[, 3])
+    expect_equal(model.actual$coefficients.pval, coefficients[, 4])
 })
