e31ede6f0c52cc02fa5fa7c2c19ef1a4303342f9
raholm <rasmus.holm723@gmail.com>
2016-09-20 21:18:04 +0200

    Updated documentation for LinearRegressionModel to be more consistent with the terminoloy. Implemented the summary method.

--- Lab4/R/linear_regression_model.R
+++ Lab4/R/linear_regression_model.R
@@ -5,18 +5,18 @@
 #' @field call Object of class \code{"character"}. A string representation of the calling code.
 #' @field coefficients Object of class \code{"list"}. A list containing information about the estimated coefficients.
 #' \describe{
-#' \item{\code{val}:}{Object of class \code{"numeric"}. The estimated parameters.}
-#' \item{\code{se}:}{Object of class \code{"numeric"}. The standard errors of the parameters.}
-#' \item{\code{var}:}{Object of class \code{"numeric"}. The variance of the parameters.}
-#' \item{\code{tval}:}{Object of class \code{"numeric"}. The t-values of the the parameters.}
-#' \item{\code{pval}:}{Object of class \code{"numeric"}. The p-values of the parameters.}
+#' \item{\code{val}:}{Object of class \code{"numeric"}. The estimated coefficients.}
+#' \item{\code{se}:}{Object of class \code{"numeric"}. The standard errors of the coefficients.}
+#' \item{\code{var}:}{Object of class \code{"numeric"}. The variance of the coefficients.}
+#' \item{\code{tval}:}{Object of class \code{"numeric"}. The t-values of the the coefficients.}
+#' \item{\code{pval}:}{Object of class \code{"numeric"}. The p-values of the coefficients.}
 #' }
 #' @field residuals Ojbect of class \code{"list"}. A list containing information about the residuals.
 #' \describe{
 #' \item{\code{val}:}{Object of class \code{"numeric"}. The residuals.}
 #' \item{\code{var}:}{Object of class \code{"numeric"}. The variance of the residuals.}
 #' }
-#' @field fitted.values Object of class \code{"numeric"}. The fitted values of y using the model's parameters.
+#' @field fitted.values Object of class \code{"numeric"}. The fitted values of y using the model's coefficients.
 #' @field df Object of class \code{"numeric"}. The number of degrees of freedom in the model.
 #'
 #' @import ggplot2
@@ -69,6 +69,46 @@ LinearRegressionModel$methods(list(
                           },
                           summary = function() {
                               "Shows a summary of the model."
+                              format_number <- function(number, decimals, ...) {
+                                  formatted <- as.numeric(format(round(number, decimals), nsmall=decimals, ...))
+                                  names(formatted) <- names(number)
+                                  return(formatted)
+                              }
+
+                              min <- format_number(min(residuals$val), 4)
+                              quartile1 <- format_number(quantile(residuals$val)[2], 4)
+                              median <- format_number(median(residuals$val), 4)
+                              quartile3 <- format_number(quantile(residuals$val)[4], 4)
+                              max <- format_number(max(residuals$val), 4)
+                              se <- format_number(sqrt(residuals$var), 4)
+                              names <- c("min", "1Q", "median", "3Q", "max")
+
+                              resid_statistics <- c(min, quartile1, median, quartile3, max)
+                              names(resid_statistics) <- names
+
+                              rownames <- names(coefficients$val)
+                              colnames <- c("Estimate", "Std. Error", "t value", "p value")
+                              coef_statistics <- matrix(c(format_number(coefficients$val, 4),
+                                                          format_number(coefficients$se, 4),
+                                                          format_number(coefficients$tval, 4),
+                                                          coefficients$pval),
+                                                        byrow=FALSE,
+                                                        nrow=length(coefficients$val),
+                                                        dimnames=list(rownames, colnames))
+
+                              cat("\nCall:\n")
+                              cat(call)
+                              cat("\n\n")
+                              cat("Residuals:\n")
+                              base::print(resid_statistics)
+                              cat("\n")
+                              cat("Coefficients:\n")
+                              base::print(coef_statistics)
+                              cat("\nResidual standard error: ")
+                              cat(se)
+                              cat(" on ")
+                              cat(df)
+                              cat(" degrees of freedom\n\n")
                           },
                           print = function() {
                               "Prints the model."
