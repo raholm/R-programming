c09b5a5536710b563a4e42b632f6de727b0b9d50
Rasmus Holm <rasmus.holm723@gmail.com>
2016-09-21 12:02:54 +0200

    Removed minor grids from the plots.

--- Lab4/R/linear_regression_model_internal.R
+++ Lab4/R/linear_regression_model_internal.R
@@ -160,7 +160,8 @@
            ylab(ylab) +
            theme_bw() +
            theme(plot.title=element_text(hjust=0.5),
-                 plot.background = element_rect(fill = "#54D8E0")) +
+                 plot.background = element_rect(fill = "#54D8E0"),
+                 panel.grid.minor = element_blank()) +
            geom_point(data=data, aes_string(x="x", y="y")) +
            geom_smooth(data=data, aes_string(x="x", y="y"), method="loess",
                        color="red", se=FALSE) +
