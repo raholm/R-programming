dc433f113f0b35bdc607000b8ad15c41f2b630a6
raholm <rasmus.holm723@gmail.com>
2016-09-27 22:18:57 +0200

    Removed redundant text.

--- Lab5/tests/testthat/test_uclassifier_internal.R
+++ Lab5/tests/testthat/test_uclassifier_internal.R
@@ -4,40 +4,40 @@ context("UClassifier Internals")
 test_that("Invalid text input is invalid", {
     expect_error(.check.text_input())
     expect_error(.check.text_input(1))
-    expect_error(.check.text_input(data.frame("THIS IS INVALID INPUT")))
-    expect_error(.check.text_input(list("THIS IS INVALID INPUT")))
+    expect_error(.check.text_input(data.frame("INVALID INPUT")))
+    expect_error(.check.text_input(list("INVALID INPUT")))
 })
 
 test_that("Valid text input is correctly formatted", {
-    expect_equal(.format.text_input("THIS IS A TEST"),
-                 list(texts="THIS IS A TEST"))
-    expect_equal(.format.text_input(list(text="THIS IS A TEST")),
-                 list(texts="THIS IS A TEST"))
-    expect_equal(.format.text_input(data.frame(text="THIS IS A TEST")),
-                 list(texts="THIS IS A TEST"))
+    expect_equal(.format.text_input("TEST"),
+                 list(texts="TEST"))
+    expect_equal(.format.text_input(list(text="TEST")),
+                 list(texts="TEST"))
+    expect_equal(.format.text_input(data.frame(text="TEST")),
+                 list(texts="TEST"))
 
-    expect_equal(.format.text_input(c("THIS IS A TEST1", "THIS IS A TEST2")),
-                 list(texts=c("THIS IS A TEST1", "THIS IS A TEST2")))
-    expect_equal(.format.text_input(list(text=c("THIS IS A TEST1", "THIS IS A TEST2"))),
-                 list(texts=c("THIS IS A TEST1", "THIS IS A TEST2")))
-    expect_equal(.format.text_input(data.frame(text=c("THIS IS A TEST1", "THIS IS A TEST2"))),
-                 list(texts=c("THIS IS A TEST1", "THIS IS A TEST2")))
+    expect_equal(.format.text_input(c("TEST1", "TEST2")),
+                 list(texts=c("TEST1", "TEST2")))
+    expect_equal(.format.text_input(list(text=c("TEST1", "TEST2"))),
+                 list(texts=c("TEST1", "TEST2")))
+    expect_equal(.format.text_input(data.frame(text=c("TEST1", "TEST2"))),
+                 list(texts=c("TEST1", "TEST2")))
 })
 
 test_that("Valid text input is correctly parsed to JSON", {
-    expect_equal(.to_json.text_input("THIS IS A TEST"),
-                 toJSON(list(texts="THIS IS A TEST")))
-    expect_equal(.to_json.text_input(list(text="THIS IS A TEST")),
-                 toJSON(list(texts="THIS IS A TEST")))
-    expect_equal(.to_json.text_input(data.frame(text="THIS IS A TEST")),
-                 toJSON(list(texts="THIS IS A TEST")))
+    expect_equal(.to_json.text_input("TEST"),
+                 toJSON(list(texts="TEST")))
+    expect_equal(.to_json.text_input(list(text="TEST")),
+                 toJSON(list(texts="TEST")))
+    expect_equal(.to_json.text_input(data.frame(text="TEST")),
+                 toJSON(list(texts="TEST")))
 
-    expect_equal(.to_json.text_input(c("THIS IS A TEST1", "THIS IS A TEST2")),
-                 toJSON(list(texts=c("THIS IS A TEST1", "THIS IS A TEST2"))))
-    expect_equal(.to_json.text_input(list(text=c("THIS IS A TEST1", "THIS IS A TEST2"))),
-                 toJSON(list(texts=c("THIS IS A TEST1", "THIS IS A TEST2"))))
-    expect_equal(.to_json.text_input(data.frame(text=c("THIS IS A TEST1", "THIS IS A TEST2"))),
-                 toJSON(list(texts=c("THIS IS A TEST1", "THIS IS A TEST2"))))
+    expect_equal(.to_json.text_input(c("TEST1", "TEST2")),
+                 toJSON(list(texts=c("TEST1", "TEST2"))))
+    expect_equal(.to_json.text_input(list(text=c("TEST1", "TEST2"))),
+                 toJSON(list(texts=c("TEST1", "TEST2"))))
+    expect_equal(.to_json.text_input(data.frame(text=c("TEST1", "TEST2"))),
+                 toJSON(list(texts=c("TEST1", "TEST2"))))
 })
 
 test_that("Invalid class name is invalid", {
