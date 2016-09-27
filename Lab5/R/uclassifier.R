5692673b731a3a3b1a29998db62a17aa9219e647
raholm <rasmus.holm723@gmail.com>
2016-09-27 23:52:12 +0200

    Added minimum documentation to UClassifier Reference Class.

--- Lab5/R/uclassifier.R
+++ Lab5/R/uclassifier.R
@@ -1,3 +1,15 @@
+#' UClassifier
+#'
+#' UClassifier is a text classifier that uses the rest-API provided by uclassify.com
+#'
+#' @field classifier_name Object of class \code{"character"}. The name of the classifier.
+#' @field username Object of class \code{"character"}. Your username on the website.
+#' @field read_token Object of class \code{"character"}. Your read token to access the API.
+#' @field write_token Object of class \code{"character"}. Your write token to access the API.
+#'
+#' @exportClass UClassifier
+#'
+#' @source \url{https://uclassify.com/docs}
 UClassifier <- setRefClass("UClassifier",
                            fields=list(
                                classifier_name="character",
@@ -9,34 +21,35 @@ UClassifier <- setRefClass("UClassifier",
 
 ## Constructor
 UClassifier$methods(list(
-                initialize=function(classifier_name, username, read_token, write_token, ...) {
+                initialize = function(classifier_name, username, read_token, write_token, ...) {
+                    .initialize(.self, classifier_name, username, read_token, write_token)
                     callSuper(...)
                 }
             ))
 
 ## Read Calls
 UClassifier$methods(list(
-                get_information=function() {
+                get_information = function() {
                     return(.get_information(.self))
                 },
-                classify=function(text) {
+                classify = function(text) {
                 },
-                get_keywords=function(text) {
+                get_keywords = function(text) {
                 }
             ))
 
 ## Write Calls
 UClassifier$methods(list(
-                add_class=function(class) {
+                add_class = function(class) {
                     "Cache the class"
                 },
-                remove_class=function(class) {
+                remove_class = function(class) {
                     "Don't forget to remove the class from the cache"
                 },
-                train=function(text, class) {
+                train = function(text, class) {
                     "Check that class exists using the cache in order to reduce the number of API calls."
                 },
-                untrain=function(text, class) {
+                untrain = function(text, class) {
                     "Check that class exists using the cache in order to reduce the number of API calls."
                 }
             ))
