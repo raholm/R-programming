f96794b5bf75c3f779687b81e463591a3b9a8904
raholm <rasmus.holm723@gmail.com>
2016-09-27 21:26:50 +0200

    Created a skeleton for the UClassifier Reference Class.

---
+++ Lab5/R/uclassifier.R
@@ -0,0 +1,46 @@
+UClassifier <- setRefClass("UClassifier",
+                           fields=list(
+                               classifier_name="character",
+                               username="character",
+                               read_token="character",
+                               write_token="character",
+                               cache="list"
+                           ))
+
+## Constructor
+UClassifier$methods(list(
+                initialize=function(classifier_name, username, read_token, write_token, ...) {
+                    callSuper(...)
+                }
+            ))
+
+## Read Calls
+UClassifier$methods(list(
+                get_information=function() {
+
+                },
+                classify=function(text) {
+                    "Text input should look like this: list(texts=c(\"text 1 to process", "text 2 to process\"))"
+                },
+                get_keywords=function(text) {
+                    "Text input should look like this: list(texts=c(\"text 1 to process", "text 2 to process\"))"
+                }
+            ))
+
+## Write Calls
+UClassifier$methods(list(
+                add_class=function(class_name) {
+                    "Cache the class_name"
+                },
+                remove_class=function(class_name) {
+                    "Don't forget to remove the class_name from the cache"
+                },
+                train=function(text, class_name) {
+                    "Check that class_name exists using the cache in order to reduce the number of API calls."
+                    "Text input should look like this: list(texts=c(\"text 1 to process", "text 2 to process\"))"
+                },
+                untrain=function(text, class_name) {
+                    "Check that class_name exists using the cache in order to reduce the number of API calls."
+                    "Text input should look like this: list(texts=c(\"text 1 to process", "text 2 to process\"))"
+                }
+            ))
