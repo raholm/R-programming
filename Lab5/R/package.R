791e8a13f2fe412386614f3a24b8b055f525d324
Rasmus Holm <rasmus.holm723@gmail.com>
2016-09-29 12:54:23 +0200

    Added more documentation.

---
+++ Lab5/R/package.R
@@ -0,0 +1,10 @@
+#' UClassify
+#'
+#' A wrapper around the rest-API provided by uclassify.com that makes it possible to create classifiers for text data. 
+#' @author Rasmus Holm
+#' @author Zhendong Wang
+#' @author Karolina Ziomek
+#'
+#' @docType package
+#' @name Lab5
+NULL
