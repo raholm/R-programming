a0120e304d4e03e07a73086e95e0a0e63590ca85
raholm <rasmus.holm723@gmail.com>
2016-10-02 20:57:05 +0200

    Added the data set and skeleton of the functions.

---
+++ Lab6/R/knapsack.R
@@ -0,0 +1,8 @@
+knapsack_brute_force <- function(x, W) {
+}
+
+knapsack_dynamic <- function(x, W) {
+}
+
+knapsack_greedy <- function(x, W) {
+}
---
+++ Lab6/R/knapsack_objects.R
@@ -0,0 +1,16 @@
+#' Knapsack Objects
+#'
+#' A data set containing knapsack objects
+#'
+#' @format A data frame with two variables.
+#' \describe{
+#' \item{w}{The weights of the objects.}
+#' \item{v}{The values of the objects.}
+#' }
+#'
+#' @name knapsack_objects
+#' @export
+set.seed(42)
+n <- 2000
+knapsack_objects <- data.frame(w=sample(1:4000, size=n, replace=TRUE),
+                               v=runif(n=n, 0, 10000))
