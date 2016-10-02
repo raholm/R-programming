c0ac4562984f0c772faffa249ee8eb591c06a2d6
raholm <rasmus.holm723@gmail.com>
2016-10-02 21:56:43 +0200

    Implemented the brute force solution.

--- Lab6/R/knapsack.R
+++ Lab6/R/knapsack.R
@@ -1,4 +1,31 @@
 knapsack_brute_force <- function(x, W) {
+    n <- nrow(x)
+
+    best_value <- -Inf
+    best_weight <- Inf
+    best_choice <- NULL
+
+    for (i in 1:2^n) {
+        bitstring <- intToBits(i)
+
+        current_weight <- 0
+        current_value <- 0
+
+        for (k in 1:n) {
+            if (bitstring[k] == 1) {
+                current_weight <- current_weight + x$w[k]
+                current_value <- current_value + x$v[k]
+            }
+        }
+
+        if (current_value > best_value && current_weight < W) {
+            best_value <- current_value
+            best_weight <- current_weight
+            best_choice <- bitstring
+        }
+    }
+
+    return(list(value=as.integer(best_value + 0.5), weight=best_weight, elements=which(best_choice == 1)))
 }
 
 knapsack_dynamic <- function(x, W) {
