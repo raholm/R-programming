44a8858cca61146fd25f4608840b9ee338ff42d4
simon9403 <crossgw94@gmail.com>
2016-10-03 15:04:18 +0200

    Lab6 discription

---
+++ Lab6/R/Lab6.R
@@ -0,0 +1,11 @@
+#' Solving the knapsack problem.
+#'
+#' This package contains three different functions for solving what is called the knapsack problem.
+#'
+#' @author Rasmus Holm
+#' @author Zhendong Wang
+#' @author Karolina Ziomek
+#' 
+#' @docType package
+#' @name Lab6
+NULL
