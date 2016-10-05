3fd4adb6b9b4bac58a6e307153b39db0fc3c00d2
simon9403 <crossgw94@gmail.com>
2016-10-05 14:46:07 +0200

    change the number to adapt the Question of "How long time does it takes to run the algorithm for n = 1000000 objects?" in 1.1.4.

--- Lab6/R/knapsack_objects.R
+++ Lab6/R/knapsack_objects.R
@@ -11,6 +11,6 @@
 #' @name knapsack_objects
 #' @export
 set.seed(42)
-n <- 2000
+n <- 1000000
 knapsack_objects <- data.frame(w=sample(1:4000, size=n, replace=TRUE),
                                v=runif(n=n, 0, 10000))
