02a3b82e294009a27914e2cbeb4e07dc1da02ac9
simon9403 <crossgw94@gmail.com>
2016-10-06 22:33:06 +0200

    Change the data objects into a function so it could generate different results for different "n".Update vignette

--- Lab6/R/knapsack_objects.R
+++ Lab6/R/knapsack_objects.R
@@ -10,7 +10,9 @@
 #'
 #' @name knapsack_objects
 #' @export
+knapsack_objects <- function(n){
 set.seed(42)
-n <- 1000000
-knapsack_objects <- data.frame(w=sample(1:4000, size=n, replace=TRUE),
+knapsack_df <- data.frame(w=sample(1:4000, size=n, replace=TRUE),
                                v=runif(n=n, 0, 10000))
+return(knapsack_df)
+}
