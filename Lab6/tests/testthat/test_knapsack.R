193c399422356f225fb1b2debdbecb29a4d50ada
simon9403 <crossgw94@gmail.com>
2016-10-06 23:34:21 +0200

    Updated the test with 'greedy' function changing.

--- Lab6/tests/testthat/test_knapsack.R
+++ Lab6/tests/testthat/test_knapsack.R
@@ -99,18 +99,18 @@ test_that("Dynamic solution using cpp is correct", {
 
 test_that("Greedy solution is correct", {
     actual <- knapsack_greedy(x = test_knapsack_objects[1:800,], W = 3500)
-    expect_equal(actual$value, 192647 + 2277)
+    expect_equal(actual$value, 192647)
     expect_equal(actual$elements, c(92, 574, 472, 80, 110, 537, 332, 117, 37, 776,
                                     577, 288, 234, 255, 500, 794, 55, 290, 436, 346,
                                     282, 764, 599, 303, 345, 300, 243, 43, 747, 35,
-                                    77, 229, 719, 564, 401))
+                                    77, 229, 719, 564))
 
     actual <- knapsack_greedy(x = test_knapsack_objects[1:1200,], W = 2000)
-    expect_equal(actual$value, 212337 + 961)
+    expect_equal(actual$value, 212337)
     expect_equal(actual$elements, c(92, 574, 472, 80, 110, 840, 537, 1000, 332, 117,
                                     37, 1197, 1152, 947, 904, 776, 577, 288, 1147,
                                     1131, 234, 255, 1006, 833, 1176, 1092, 873, 828,
-                                    1059, 500, 1090, 794, 1033, 1134))
+                                    1059, 500, 1090, 794, 1033))
 })
 
 test_that("Greedy solution using cpp is correct", {
