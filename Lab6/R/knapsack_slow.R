6bafb067756fd8d7bab31771f1662c173a4387ca
raholm <rasmus.holm723@gmail.com>
2016-10-07 16:10:59 +0200

    Added file with a slower version of brute force.

---
+++ Lab6/R/knapsack_slow.R
@@ -0,0 +1,29 @@
+knapsack_brute_force_R_old <- function(x, W) {
+    n <- nrow(x)
+
+    best_value <- -Inf
+    best_weight <- 0
+    best_choice <- NULL
+
+    for (i in 1:(2^n - 1)) {
+        bitstring <- intToBits(i)
+
+        current_weight <- 0
+        current_value <- 0
+
+        for (k in 1:n) {
+            if (bitstring[k] == 1) {
+                current_weight <- current_weight + x$w[k]
+                current_value <- current_value + x$v[k]
+            }
+        }
+
+        if (current_value > best_value && current_weight < W) {
+            best_value <- current_value
+            best_weight <- current_weight
+            best_choice <- bitstring
+        }
+    }
+
+    return(list(value=as.integer(best_value + 0.5), weight=best_weight, elements=which(best_choice == 1)))
+}
